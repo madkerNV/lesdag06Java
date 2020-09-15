7952a400046916d8f87bf8e2531350bf2d32db78
madkerNV <madbelit@gmail.com>
2020-09-15 13:12:53 +0200

    project aangepast naar de les

--- src/test/java/Lesdag06testen.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class Lesdag06testen {
-
-    @Test
-    void experimentMetAssert(){
-
-    }
-}
---
+++ src/test/java/org/example/madjid/lesdag06.java
@@ -0,0 +1,11 @@
+package org.example.madjid;
+
+import org.junit.jupiter.api.Test;
+
+public class lesdag06 {
+
+    @Test
+    void experimentMetStringObject(){
+        String s1 = "Dit is een string";
+    }
+}
