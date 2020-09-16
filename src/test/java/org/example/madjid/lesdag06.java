5c5843eb59eef6c06c13e4086479040b696d1eb1
madkerNV <madbelit@gmail.com>
2020-09-16 11:22:19 +0200

    test rechterDriehoek

--- src/test/java/org/example/madjid/lesdag06.java
+++ src/test/java/org/example/madjid/lesdag06.java
@@ -58,6 +58,45 @@ void printDriehoekMetPuntNaarBoven() {
 }
 
 
+    @Test
+    void rechterDriehoek() {
+        int size = 5;
+        printAt();
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            rechterDriehoekOpbouwen(i);
+            System.out.println();
+        }
+        for (int i = 0; i < size-1; i++) { //hier zat het probleem
+            rechterDriehoekAfbouwen(i, size-1);
+            System.out.println();
+        }
+        printAt();
+    }
+
+    void rechterDriehoekOpbouwen(int i) {
+        printAt();
+        for (int j = 0; j <= i; j++) {
+            printStar();
+        }
+        printAt();
+    }
+    void rechterDriehoekAfbouwen(int i, int size) {
+        printAt();
+        for (int j = size; i < j; j = j -1){
+            printStar();
+        }
+        printAt();
+    }
+
+    void printStar() {
+        System.out.print("* ");
+    }
+    void printAt() {
+        System.out.print("@");
+    }
+
+
 }
 
 
